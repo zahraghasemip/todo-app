34e9b4cce99c8ba4b7048938180fd3b7338c835b
zahraghasemip <zahraghasemi2487@gmail.com>
2023-07-30 22:55:52 +0330

    union types

---
+++ union.ts
@@ -0,0 +1,13 @@
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 == "number" && typeof input2 == "number") {
+    result = input1 + input2;
+  } else {
+    result = input1.toString() + input2.toString();
+  }
+  return result;
+}
+
+const combineNumbers = combine(24, 29);
+const combineStrings = combine("shabi", "gh");
+console.log(combineNumbers, combineStrings);
