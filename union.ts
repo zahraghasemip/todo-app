57f70488796157d6e4e5d2c687ba59ae0761ee46
zahraghasemip <zahraghasemi2487@gmail.com>
2023-08-05 11:59:38 +0330

    literal types

--- union.ts
+++ union.ts
@@ -1,13 +1,23 @@
-function combine(input1: number | string, input2: number | string) {
+function combine(
+  input1: number | string,
+  input2: number | string,
+  resulttype: "as-number" | "as-string",
+) {
   let result;
   if (typeof input1 == "number" && typeof input2 == "number") {
     result = input1 + input2;
   } else {
     result = input1.toString() + input2.toString();
   }
-  return result;
+  if (resulttype === "as-number") {
+    return +result;
+  } else {
+    return result.toString();
+  }
 }
 
-const combineNumbers = combine(24, 29);
-const combineStrings = combine("shabi", "gh");
-console.log(combineNumbers, combineStrings);
+const combineNumbers = combine("3", "2", "as-string");
+const combineNumbers2 = combine("2", "2", "as-number");
+
+const combineStrings = combine("shabi", "gh", "as-string");
+console.log(combineNumbers, combineStrings, combineNumbers2);
